64e093258293cf830da7cca48ebc353f38f7dc58
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-07-11 12:11:10 +0530

    added a basic main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 mod envy;
 
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(name = "envy")]
+#[command(author = "Tanmay Chaudhry <tanmay.chaudhry@gmail.com")]
+#[command(version = "0.1.0")]
+#[command(about="A tool to automagically create 'executable' packages for your scripts.", long_about=None)]
+struct Args {
+    // Project Root
+    #[arg(long, short, default_value_os_t = PathBuf::from("."))]
+    project_root: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let pack = envy::package::PackBuilder::new(args.project_root)?.build()?;
+    println!("{:?}", pack);
+    Ok(())
 }
